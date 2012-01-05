680e0992060855158b89e8282ba532518e042456
Jake Stauch <jake.stauch@neurospire.com>
2012-01-04 21:50:16 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'sqlite3', '1.3.3'
+
+group :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3984e62483a102e3d113bb5b02c37a500dc6256dd621e04782ae63742f6a28547989f660d91995b2ed50c3480e6f9f3172aaf118306949fee5db6e72313803ab'
